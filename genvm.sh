66bdc9ef4d345714f84965a6a2fb753d01b016ed
Vincent Davis Jr <vince@underview.tech>
2025-02-21 13:32:24 -0500

    genvm: add initial virt-install function
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- genvm.sh
+++ genvm.sh
@@ -3,19 +3,56 @@
 SUCCESS=0
 FAILURE=1
 
+CDIR="$(pwd)"
+VM_STORAGE="${CDIR}/vm-storage"
+
 VM_SIZE=0
 VM_NAME=""
 INSTALLER_FILE=""
 
+gen_vm () {
+	mkdir -p "${VM_STORAGE}"
+
+	local domain="${VM_STORAGE}/${VM_NAME}.xml"
+	local qed_file="${VM_STORAGE}/${VM_NAME}.qed"
+
+	local vm_size=$(stat --dereference --format="%s" "${INSTALLER_FILE}")
+	vm_size=$((vm_size / 1024 / 1024 / 1024))
+
+	virt-install \
+		--virt-type kvm \
+		--os-variant generic \
+		--name "${VM_NAME}" \
+		--cpu host \
+		--vcpus 8 \
+		--memory 8196 \
+		--disk path="${INSTALLER_FILE}",size="${vm_size}",device="disk",bus="usb",format="raw" \
+		--disk path="${qed_file}",size="${VM_SIZE}",device="disk",bus="sata",format="qed" \
+		--print-xml > "${domain}"
+	if [ $? -ne 0 ]; then
+		echo "[x] virt-install ${VM_NAME} failed"
+		return $FAILURE
+	fi
+
+	virsh define "${domain}"
+	if [ $? -ne 0 ]; then
+		echo "[x] virsh define ${domain} failed"
+		return $FAILURE
+	fi
+
+	return $SUCCESS
+}
+
+
 help_msg () {
-	fname=$1
+	fname="$1"
 	printf "Usage: ${fname} [options]\n"
 	printf "Example: ${fname} --installer-file <installer image> --vm-name udoo-bolt --vm-size 32\n"
 	printf "Options:\n"
-	printf "\t-i, --installer-file <installer image>" ; printf "\tLiveusb installer image.\n"
-	printf "\t-n, --vm-name <vm name>               " ; printf "\tName of the virtual machine.\n"
-	printf "\t-s, --vm-size <vm size>               " ; printf "\tSize in gigabytes of the virtual machine.\n"
-	printf "\t-h, --help                            " ; printf "\tSee this message.\n"
+	printf "\t-i, --installer-file <installer image>" ; printf "\tLiveusb installer image\n"
+	printf "\t-n, --vm-name <vm name>               " ; printf "\tName of the virtual machine\n"
+	printf "\t-s, --vm-size <vm size>               " ; printf "\tSize of the virtual machine (In GiB)\n"
+	printf "\t-h, --help                            " ; printf "\tSee this message\n"
 }
 
 
@@ -65,11 +102,12 @@ done
 
 # Check all arguments are passed
 if [ -z "${INSTALLER_FILE}" ] || \
-   [ -z "${VM_NAME}" ] || \
-   [ "${VM_SIZE}" -eq 0 ];
+   [ -z "${VM_NAME}"        ] || \
+   [ "${VM_SIZE}" -eq 0     ];
 then
 	exit_err_help
 fi
 
+gen_vm || exit 1
 
 exit 0
